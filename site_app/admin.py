429cfd0bef79d7c4b54180fc7013cdc8cf3cf11e
Gareth M. <gkm25@protonmail.com>
2025-03-25 16:34:22 -0500

    signup and login functions working, admin user/dashboard configured

--- site_app/admin.py
+++ site_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Order, Address, Product
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(Product)
---
+++ site_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+	path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.home, name="home"),
+    path('signup', views.signup, name="signup"),
+    path('profile', views.profile, name='profile')
+]
