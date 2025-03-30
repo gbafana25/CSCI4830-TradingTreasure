60fe17e5e9067514cbd57fee11fd242a911c45e3
Gareth M. <gkm25@protonmail.com>
2025-03-30 02:49:21 -0500

    added address update form, switched forms to show individual inputs in templates

--- site_app/forms.py
+++ site_app/forms.py
@@ -2,15 +2,20 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-class SignupForm(UserCreationForm):
-	#username = forms.CharField()
-	#password = forms.CharField()
-	#email = forms.CharField()
-	#location = forms.CharField()
-	#phone_number = forms.IntegerField()
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-        fields = ("username", "email", "location", "phone_number")
+class SignupForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+    email = forms.CharField()
+    location = forms.CharField()
+    phone_number = forms.IntegerField()
+
+class AddressForm(forms.Form):
+        address_line1 = forms.CharField()
+        address_line2 = forms.CharField()
+        city = forms.CharField()
+        state = forms.CharField()
+        zip_code = forms.IntegerField()
+
         
 class LoginForm(forms.Form):
     username = forms.CharField()
