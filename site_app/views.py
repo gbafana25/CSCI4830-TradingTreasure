3224a1282c96f372b5f7adb863f795ced4026239
Brayden76 <bmlm11135@gmail.com>
2025-04-01 08:19:06 -0500

    CRUD functions

--- site_app/views.py
+++ site_app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ItemForm
+from .models import Item
+from django.shortcuts import get_object_or_404, redirect
 
-# Create your views here.
+def create_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm()
+    return render(request, 'items/item_form.html', {'form': form})
+
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'items/item_list.html', {'items': items})
+
+
+def update_item(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'items/item_form.html', {'form': form})
+
+def delete_item(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'items/item_confirm_delete.html', {'item': item})
+
--- trading_treasure/urls.py
+++ trading_treasure/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.item_list, name='item_list'),
+    path('item/new/', views.create_item, name='create_item'),
+    path('item/<int:pk>/edit/', views.update_item, name='update_item'),
+    path('item/<int:pk>/delete/', views.delete_item, name='delete_item'),
 ]
