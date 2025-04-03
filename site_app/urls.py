accc352328f7de51d5146e13bf4f3fcac84dcf6b
Esteven Valencia <evalencia@unomaha.edu>
2025-04-02 19:24:22 -0500

    Adding an Items page to display all the items to sell and buy

--- site_app/urls.py
+++ site_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('profile', views.profile, name='profile'),
     path('update_address', views.update_address, name='update_address')
+    path('Items', view.item, name='itmes')
 ]
