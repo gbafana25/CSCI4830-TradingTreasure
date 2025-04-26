c8d8ba7d200c6127c026648d31f02a06618d2575
Gareth M. <gkm25@protonmail.com>
2025-04-26 17:43:18 -0500

    fixed pagination, buy item page redirects, buyer orders show up on account page

--- site_app/urls.py
+++ site_app/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [
 	path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name="home"),
+    #path('<int:page>/', views.home, name="home"),
     path('signup', views.signup, name="signup"),
     path('profile', views.profile, name='profile'),
     path('update_address', views.update_address, name='update_address'),
     path('page2/', views.page2, name="page2"),
     path('page3/', views.page3, name="page3"),
-    path('buy-item/<uuid:id>/', views.buy_item, name="buy-item")
+    path('buy-item/<uuid:id>/', views.buy_item, name="buy-item"),
+    path('place-order/<uuid:id>/', views.place_order, name="place-order")
 ]
